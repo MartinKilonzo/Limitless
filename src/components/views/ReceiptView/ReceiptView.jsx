12826a2a4e0a63e36c069cbec532cd22bc6371cb
Howard-F <howard.fung@mail.utoronto.ca>
2016-09-25 09:45:24 -0400

    styles

--- src/components/views/ReceiptView/ReceiptView.jsx
+++ src/components/views/ReceiptView/ReceiptView.jsx
@@ -3,15 +3,33 @@
 import React from 'react';
 
 var NewComponent = React.createClass({
+    
   render: function() {
+    const styles = {
+        upload: {
+            textAlign: 'center',
+            fontFamily: 'Roboto, sans-serif',
+        },
+        uploadBut: {
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            width: '50%'
+        },
+        btn: {
+            width: '20%',
+            textAlign: 'center'
+        }
+    };
     return (
-      <div>
-        <title>Upload new File</title>
-        <h1>Upload new File</h1>
-        <form action="http://127.0.0.1:5000/upload" method="post" encType="multipart/form-data">
-          <p><input type="file" name="file" />
-            <input type="submit" defaultValue="Upload" />
-          </p></form>
+      <div style={styles.foo}>
+        <title>Upload new File</title> 
+        <h1 id = "upload" style={styles.upload}>Upload new File</h1>
+        <form action="http://127.0.0.1:5000/upload" method="post" encType="multipart/form-data" style={styles.uploadBut}>
+        <input style={styles.foo} type="file" name="file" />
+        <div>    </div>
+        <input style={styles.btn} type="submit" defaultValue="Upload" />
+        </form>
       </div>
     );
   }
